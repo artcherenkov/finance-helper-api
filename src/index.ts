827d080b462bb5b09e242fc23bc346cbfc3f3c15
Artem Cherenkov <art.cherenkov@gmail.com>
2021-11-17 11:49:31 +0700

    feat: configure index file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express from "express";
+import bodyParser from "body-parser";
 
-const PORT = 3030;
+import db from "./db";
+
+import error from "./middlewares/error";
+import { register } from "./controllers/user";
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+app.get("/", (req, res) => res.send("Hello world"));
+
+app.post("/user", register);
+
+app.use(error);
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
